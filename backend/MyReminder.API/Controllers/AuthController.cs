dde04f5bd449829ae326656bb0d13edd1b2657bf
gabriel gallardo <ggallardo@chocobru.local>
2026-08-13 16:38:53 -0400

    fix: agregada la directiva using Microsoft.AspNetCore.Authorization en AuthController.cs para corregir compilación en Docker de Render

--- backend/MyReminder.API/Controllers/AuthController.cs
+++ backend/MyReminder.API/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyReminder.API.Data;
